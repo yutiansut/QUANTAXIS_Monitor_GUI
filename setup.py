3da6fbffeb7ba899c5e8f2a5cb13ced00dd1328d
yutiansut <yutiansut@qq.com>
2019-01-15 02:51:17 +0800

    #

---
+++ setup.py
@@ -0,0 +1,74 @@
+# coding=utf-8
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2016-2017 yutiansut/QUANTAXIS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import codecs
+import io
+import os
+import re
+import sys
+import webbrowser
+import platform
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+def read(fname):
+
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+NAME = "QUANTAXIS_CRAWLY"
+VERSION = '1.0'
+AUTHOR = 'yutiansut'
+PACKAGES = ["QUANTAXIS_CRAWLY"]
+DESCRIPTION = "QUANTAXIS CRAWLY: QUANTAXIS SPIDER"
+KEYWORDS = ["quantaxis", "quant", "finance", "Backtest", 'Framework']
+AUTHOR_EMAIL = "yutiansut@qq.com"
+URL = "https://github.com/quantaxis/quantaxis"
+LICENSE = "MIT"
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=['quantaxis', 'selenium',
+        'tornado', 'scrapy', 'pymongo>=3.7'],
+    # install_requires=requirements,
+    keywords = KEYWORDS,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = URL,
+    license = LICENSE,
+    packages = PACKAGES,
+    include_package_data = True,
+    zip_safe = True
+)
